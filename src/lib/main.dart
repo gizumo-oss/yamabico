57d597a9235e93f5535424607343bfbd481b1f1c
Takayyz <like.the.wind0306@gmail.com>
2023-07-14 14:07:50 +0900

    change: ログイン画面からアカウント登録の導線削除

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -38,6 +38,27 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return Authenticator(
       stringResolver: stringResolver,
+      authenticatorBuilder: (BuildContext context, AuthenticatorState state) {
+        switch (state.currentStep) {
+          case AuthenticatorStep.signIn:
+            return CustomScaffold(
+              state: state,
+              body: SignInForm(),
+            );
+          case AuthenticatorStep.resetPassword:
+            return CustomScaffold(
+              state: state,
+              body: ResetPasswordForm(),
+            );
+          case AuthenticatorStep.confirmResetPassword:
+            return CustomScaffold(
+              state: state,
+              body: const ConfirmResetPasswordForm(),
+            );
+          default:
+            return null;
+        }
+      },
       child: MaterialApp(
         builder: Authenticator.builder(),
         home: Scaffold(
@@ -47,6 +68,44 @@ class _MyAppState extends State<MyApp> {
           ),
         ),
       ),
+    );
+  }
+}
+
+class CustomScaffold extends StatelessWidget {
+  const CustomScaffold({
+    super.key,
+    required this.state,
+    required this.body,
+    this.footer,
+  });
+
+  final AuthenticatorState state;
+  final Widget body;
+  final Widget? footer;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              // App logo
+              const Padding(
+                padding: EdgeInsets.only(top: 32),
+                child: Center(child: FlutterLogo(size: 100)),
+              ),
+              Container(
+                constraints: const BoxConstraints(maxWidth: 600),
+                child: body,
+              ),
+            ],
+          ),
+        ),
+      ),
+      persistentFooterButtons: footer != null ? [footer!] : null,
     );
   }
 }
