ca9f917c79b1a4b1d872a7c4deb97fb61db9353d
Takayyz <like.the.wind0306@gmail.com>
2023-07-15 17:16:53 +0900

    chore: .envを扱えるよう対応

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_authenticator/amplify_authenticator.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
 
 import 'amplifyconfiguration.dart';
 import 'amplify_cognito_resolver.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: '.env');
+
   runApp(const MyApp());
 }
 
