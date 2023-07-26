f6fd2f7bac1f1d58492cc89b071e00df5ec851cd
Takayyz <like.the.wind0306@gmail.com>
2023-07-26 21:12:25 +0900

    feat: ログイン状態に応じてゲストトップ or 投稿一覧への遷移制御処理追加

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -3,9 +3,12 @@ import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
 import 'package:yamabico/feature/auth/presentation/login_screen.dart';
-import 'package:yamabico/home_screen.dart';
+import 'package:yamabico/guest_top_screen.dart';
+import 'package:yamabico/feature/posts/presentation/index_screen.dart';
 
 import 'feature/auth/infra/amplifyconfiguration.dart';
+
+// INFO: ディレクトリ構成参考https://qiita.com/MLLB/items/95617322a7d984b7e402
 
 void main() async {
   await dotenv.load(fileName: '.env');
@@ -21,13 +24,17 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  bool _isAuth = false;
+
   @override
   void initState() {
     super.initState();
-    _configureAmplify();
+    _configureAmplify().then((value) => {
+      _isAuthenticated().then((result) => _isAuth = result)
+    });
   }
 
-  void _configureAmplify() async {
+  Future<void> _configureAmplify() async {
     try {
       await Amplify.addPlugin(AmplifyAuthCognito());
       await Amplify.configure(amplifyconfig);
@@ -37,13 +44,19 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
+  Future<bool> _isAuthenticated() async {
+    final authState = await Amplify.Auth.fetchAuthSession();
+    return authState.isSignedIn;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/',
+      initialRoute: _isAuth ? '/posts' : '/',
       routes: {
-        '/': (context) => const HomeScreen(),
+        '/': (context) => const GuestTopScreen(),
         '/login': (context) => const LoginScreen(),
+        '/posts': (context) => const IndexScreen(),
       },
     );
   }
