ddffdce830945edb2ddf8b71964deedca174b0ee
PekolinGX <tippy.gfop@gmail.com>
2023-08-08 20:33:26 +0900

    feat: ユーザー投稿詳細画面の追加

---
+++ src/lib/feature/user/posts_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class IndexScreen extends StatelessWidget {
+  const IndexScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('ユーザー投稿詳細画面'),
+      ),
+    );
+  }
+}
