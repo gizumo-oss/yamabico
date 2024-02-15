38cefdbd4c5ea3802d29cb0e244640831ae6c4b2
Goshi_Sato <g-sato@gizumo-inc.jp>
2024-02-16 08:27:14 +0900

    chore: set_upスクリプトから不要な記述の削除

--- .make/setup_git.sh
+++ .make/setup_git.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
 git config commit.template .gitmessage
-git remote add git@github.com:gizumo-oss/yamabico.git
-
-read -p "Gitのローカル設定を行います。名字と名前をアンダースコア区切りで入力して下さい(ex. Company_Taro): " name
-git config user.name $name
-
-read -p "emailアドレスを入力して下さい: " email
-git config user.email $email
