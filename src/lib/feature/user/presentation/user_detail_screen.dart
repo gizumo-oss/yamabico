40ea4834cb6e5ebf967dc03742300bee01904db8
koru <bigrj7ao9kl@gmail.com>
2024-02-27 03:06:55 +0900

    update: ユーザー詳細画面を実装

--- src/lib/feature/user/presentation/user_detail_screen.dart
+++ src/lib/feature/user/presentation/user_detail_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:yamabico/feature/posts/presentation/index_screen.dart'; // AudioDataを利用するため
+import 'package:yamabico/component/my_app_bar.dart';
+import 'package:yamabico/feature/posts/presentation/index_screen.dart';
+import 'package:yamabico/component/audio_post.dart';
+import 'package:yamabico/component/my_music_player.dart';
 
 class UserDetailScreen extends StatelessWidget {
-  final User user; // 表示するユーザー情報
-  final List<AudioData> userPosts; // ユーザーの投稿一覧
+  final User user;
+  final List<AudioData> userPosts;
 
   const UserDetailScreen(
       {Key? key, required this.user, required this.userPosts})
@@ -12,26 +15,97 @@ class UserDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(user.name), // ユーザー名を表示
-      ),
-      body: Column(
-        children: [
-          Text(user.avatarUrl),
-          Text(user.name),
-          Expanded(
-            child: ListView(
-              children: userPosts
-                  .map((post) => ListTile(
-                        leading:
-                            Image.network(user.avatarUrl), // ユーザーのプロフィール画像を表示
-                        title: Text(post.title), // 投稿のタイトルを表示
-                      ))
-                  .toList(),
-            ),
+        appBar: const MyAppBar(),
+        body: Stack(children: [
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Center(
+                child: Column(children: [
+              Avatar(url: user.avatarUrl, width: 200, height: 200),
+              Container(
+                padding: const EdgeInsets.all(20),
+                child: Text(
+                  user.name,
+                  style: const TextStyle(fontSize: 20),
+                ),
+              ),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10, right: 10),
+                  child: Scrollbar(
+                      child: ListView.separated(
+                    itemCount: userPosts.length + 1,
+                    separatorBuilder: (context, index) {
+                      return const Divider();
+                    },
+                    itemBuilder: (context, index) {
+                      if (index < userPosts.length) {
+                        return Row(children: [
+                          SizedBox(
+                            width: 300,
+                            child: Content(
+                                title: userPosts[index].title,
+                                name: userPosts[index].user.name,
+                                time: userPosts[index].playTime,
+                                count: userPosts[index].count,
+                                date: userPosts[index].date),
+                          ),
+                          Container(
+                            width: 70,
+                            alignment: Alignment.centerRight,
+                            child: const AudioPostWidget(),
+                          )
+                        ]);
+                      }
+                      return const SizedBox(height: 100.0);
+                    },
+                  )),
+                ),
+              )
+            ])),
           ),
-        ],
-      ),
-    );
+          Container(
+            alignment: Alignment.bottomCenter,
+            child: const MyMusicPlayer(),
+          ),
+        ]));
+  }
+}
+
+class User {
+  final String _name;
+  final String _avatarUrl;
+
+  User(this._name, this._avatarUrl);
+
+  String get name => _name;
+  String get avatarUrl => _avatarUrl;
+}
+
+class AudioPostWidget extends StatefulWidget {
+  const AudioPostWidget({super.key});
+
+  @override
+  UserDetailScreenState createState() => UserDetailScreenState();
+}
+
+class UserDetailScreenState extends State<AudioPostWidget> {
+  bool _isPlaying = false;
+
+  void isPlaying() {
+    setState(() {
+      _isPlaying = !_isPlaying;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+        icon: _isPlaying
+            ? const Icon(Icons.pause_circle_filled)
+            : const Icon(Icons.play_circle_filled),
+        color: const Color.fromRGBO(124, 122, 122, 1.0),
+        iconSize: 40,
+        onPressed: () => isPlaying());
   }
 }
